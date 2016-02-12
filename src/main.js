8d04e9bec897438fb1a21446dffb1e19b8efc929
Chang Wang <garnwraly@gmail.com>
2016-02-12 02:22:36 -0500

    Update 2016-02-12T07:22:35.649Z

--- src/main.js
+++
@@ -1,6 +0,0 @@
-if(module.hot) {
-  // Accept loading this module again when a dependency updates
-  module.hot.accept();
-}
-
-require('./main.jsx');
--- test/test-helper.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-import {jsdom} from 'jsdom';
-
-const document = global.document = jsdom('<html><head></head><body></body></html>');
-const window = global.window = document.defaultView;
-global.navigator = window.navigator = {};
